8b8af44213a52dc48ab3b96fc4b1c28c09db0226
yuhsuan <>
2023-06-15 15:57:42 +0800

    test_precommit

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 df = pd.DataFrame()
 df = df.sort_values("update_at", ascending=False)
-df_latest = df.groupby(["cupid_id", "product_id"]).head(1)
+df_latest = df.groupby(["cupid_id","product_id"]).head(1)
 df_machine = df[df.source=="machine"].rename(columns={"tags": "tags_2"})
 df_machine = df_machine.groupby(["cupid_id", "product_id"]).head(1)
 df_machine = df_machine.drop(columns=["source", "update_at"])
